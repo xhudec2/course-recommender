c576d8893afe9a6fbc264104bcaeafd0915ff66e
Lukáš Hudec <xhudec2@fi.muni.cz>
2026-03-14 16:57:43 +0100

    add better printing

--- recommender/__main__.py
+++ recommender/__main__.py
@@ -2,9 +2,11 @@ from recommender.db import Database
 
 
 def main():
-    texts = ["I want a course about AI."]
+    texts = ["I want a course about how to make and use AI models."]
     db = Database()
-    print(db.query(texts))
+    query_res = db.query(texts)
+    for i, course in enumerate(query_res["metadatas"][0]):
+        print(f"{i + 1:2d}. {course['course_code']}: {course['course_name']}")
 
 
 if __name__ == "__main__":
