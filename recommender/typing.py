27513c7e6f3b7c0bcd6f789dc5a4ab91bbc16dd7
Lukáš Hudec <xhudec2@fi.muni.cz>
2026-04-08 12:03:47 +0200

    add typing

--- recommender/typing.py
+++ recommender/typing.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import TypeAlias, TypedDict
 
 from chromadb import Embeddings, IDs, Include, Metadata
 
@@ -14,3 +14,23 @@ class StrictQueryResult(TypedDict):
     metadatas: list[list[Metadata]]
     distances: list[list[float]]
     included: Include
+
+
+CourseRound: TypeAlias = dict[str, str | list[str]]
+
+
+class CourseData(TypedDict):
+    course_code: str
+    course_name: str
+    course_id: str
+    course_swedish_name: None | str
+    course_owner: None | str
+    teaching_language: None | str
+    education_cycle: None | str
+    field_of_study: None | str
+    department: None | str
+    aim: str
+    learning_outcomes: str
+    content: str
+    summary: str
+    course_rounds: list[CourseRound]
