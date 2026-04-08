ed01ca2cdcf55e8a3d7a6089757773b9761316fb
Lukáš Hudec <xhudec2@fi.muni.cz>
2026-04-08 14:47:16 +0200

    fix formatting

--- recommender/db/__main__.py
+++ recommender/db/__main__.py
@@ -1,15 +1,9 @@
 from pathlib import Path
 
-from tqdm import tqdm
-
 from recommender.db import Database
 
 if __name__ == "__main__":
     root = Path("data/all_courses_data.csv")
-
     db = Database()
-
-    # for file in tqdm(root.iterdir()):
     db.fill(root)
-
     print("Embeddings done")
