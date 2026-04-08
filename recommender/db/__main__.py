3cb657a9967360208fad0a0d9e6525166539eacf
Lukáš Hudec <xhudec2@fi.muni.cz>
2026-04-08 12:03:32 +0200

    make module runnable

--- recommender/db/__main__.py
+++ recommender/db/__main__.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 
-from db import Database
 from tqdm import tqdm
+
+from recommender.db import Database
 
 if __name__ == "__main__":
     root = Path("data/qwen8b_summaries")
 
     db = Database()
+
     for file in tqdm(root.iterdir()):
         db.fill(file)
 
